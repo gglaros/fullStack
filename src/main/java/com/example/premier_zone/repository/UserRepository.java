62ff8cf457729576b0fc8042159170b070208d25
gglaros <it22022@hua.gr>
2025-06-08 04:56:49 +0300

    Delete src directory

--- src/main/java/com/example/premier_zone/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.premier_zone.repository;
-
-import com.example.premier_zone.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
